904d91eda8be6b01fcb0ed4b586c4a7154fde0e3
brookemadison <brookeperry611@gmail.com>
2021-10-30 13:28:10 -0400

    fixed error in mongoose.connect in server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true
 });
 
